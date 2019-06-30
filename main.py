d47f7b08170f13a20ad5c6dc66b61d3a6e41e008
ochazuke1147 <ochazuke1147@gmail.com>
2019-07-01 01:07:09 +0900

    added main.py.

---
+++ main.py
@@ -0,0 +1,9 @@
+from graphillion import GraphSet as gs
+
+universe = [('東京'   , '品川'     , 6.8),
+            ('品川'   , '新宿'     , 10.6),
+            ('新宿'   , '上野'     , 11.3),
+            ('上野'   , '東京'     , 3.6),
+            ('東京'   , '四ツ谷'   , 6.6),
+            ('四ツ谷' , '新宿'     , 3.7)]
+gs.set_universe(universe)
