f30de04fbbd26bcb7fb3d03a6eab2388df823e17
ochazuke1147 <ochazuke1147@gmail.com>
2019-07-10 15:24:28 +0900

    modified files.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # jr_path
 for getting the longest path on JR lines.
+
+Below are some kinds of critical paths.
+
+## 1. Critical path on 1500 V DC lines
+
+
+## 2. Critical path on electrified lines
+
+
+## 3. Critical path on all JR lines
--- main.py
+++ main.py
@@ -13,3 +13,49 @@ print(next(paths.max_iter()))
 import pandas as pd
 
 line_list = pd.read_csv('./route_data/route.csv', sep=',')
+station_list = pd.read_csv('./route_data/st.csv', sep=',')
+stations = {}
+univ = []
+weights = {}
+
+# read station information
+for i, sta in station_list.iterrows():
+
+    stations[sta['駅名']] = sta['駅番号']
+
+# read route data
+for i, line in line_list.iterrows():
+    edge = (int(line['始発駅']), int(line['終点駅']))
+    univ.append(edge)
+    weights[edge] = line['営業キロ']
+    if i == 70:
+        break
+
+gs.set_universe(univ)
+print(weights)
+
+current = 1
+goal = 41
+
+paths = gs.paths(current, goal)
+
+max_path = next(paths.max_iter(weights))
+
+for line in max_path:
+    print(line)
+
+print(current)
+while True:
+    if current == goal:
+        break
+    for i, line in enumerate(max_path):
+        if line[0] == current:
+            current = line[1]
+            print(current)
+            del max_path[i]
+            break
+        elif line[1] == current:
+            current = line[0]
+            print(current)
+            del max_path[i]
+            break
